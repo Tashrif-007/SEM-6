bf9d668a85f5616d06828f8b705fc1c7df7a9971
tashrif-007 <tashrifpro@gmail.com>
2025-05-31 18:45:05 +0600

    added env for loan service in container

--- Distributed Systems-601/smartlibrary/loan_service/services/loanService.js
+++ Distributed Systems-601/smartlibrary/loan_service/services/loanService.js
@@ -1,15 +1,17 @@
 import axios from 'axios';
 import Loan from '../model/loan.model.js';
+import dotenv from 'dotenv';
+dotenv.config();
 const USER_SERVICE_URL = process.env.USER_SERVICE_URL;
 const BOOK_SERVICE_URL = process.env.BOOK_SERVICE_URL;
 
 export const createLoan= async(data) => {
   try {
-    const user = await axios.get(`http://localhost/api/users/${data.user_id}`, {
+    const user = await axios.get(`${USER_SERVICE_URL}/api/users/${data.user_id}`, {
       timeout: 5000,
     });
 
-    const book = await axios.get(`http://localhost/api/books/${data.book_id}`, {
+    const book = await axios.get(`${BOOK_SERVICE_URL}/api/books/${data.book_id}`, {
       timeout: 5000,
     });
     if (book.data.availableCopies <= 0) {
@@ -20,7 +22,7 @@ export const createLoan= async(data) => {
       throw new Error('User not found');
     }
 
-    await axios.patch(`http://localhost/api/books/${data.book_id}/availability`,
+    await axios.patch(`${BOOK_SERVICE_URL}/api/books/${data.book_id}/availability`,
       {
         available_copies: 1,
         operation: 'decrement',
@@ -48,7 +50,7 @@ export const returnBook = async (loanId) => {
       throw new Error('Book already returned');
     }
     await axios.patch(
-      `http://localhost/api/books/${loan.bookId}/availability`,
+      `http://${BOOK_SERVICE_URL}/api/books/${loan.bookId}/availability`,
       {
         available_copies: 1,
         operation: 'increment',
@@ -79,8 +81,8 @@ export async function getUserLoans(userId) {
     const enrichedLoans = await Promise.all(
       loans.map(async (loan) => {
         const [bookResponse, userResponse] = await Promise.all([
-          axios.get(`http://localhost/api/books/${loan.bookId}`, { timeout: 5000 }),
-          axios.get(`http://localhost/api/users/${loan.userId}`, { timeout: 5000 }),
+          axios.get(`${BOOK_SERVICE_URL}/api/books/${loan.bookId}`, { timeout: 5000 }),
+          axios.get(`${USER_SERVICE_URL}/api/users/${loan.userId}`, { timeout: 5000 }),
         ]);
 
         return {
@@ -115,8 +117,8 @@ export const getLoan = async (id) => {
       return null;
     }
     const [bookResponse, userResponse] = await Promise.all([
-      axios.get(`http://localhost/api/books/${loan.bookId}`, { timeout: 5000 }),
-      axios.get(`http://localhost/api/users/${loan.userId}`, { timeout: 5000 }),
+      axios.get(`${BOOK_SERVICE_URL}/api/books/${loan.bookId}`, { timeout: 5000 }),
+      axios.get(`${USER_SERVICE_URL}/api/users/${loan.userId}`, { timeout: 5000 }),
     ]);
     return {
       ...loan.toObject(),
@@ -136,36 +138,3 @@ export const getLoan = async (id) => {
     throw error;
   }
 }
-
-// export async function getLoan(id) {
-//   try {
-//     const loan = await prisma.issue.findUnique({
-//       where: { id },
-//     });
-
-//     if (!loan) {
-//       return null;
-//     }
-
-//     const [bookResponse, userResponse] = await Promise.all([
-//       axios.get(`${BOOK_SERVICE_URL}/api/books/${loan.bookId}`, { timeout: 5000 }),
-//       axios.get(`${USER_SERVICE_URL}/api/users/${loan.userId}`, { timeout: 5000 }),
-//     ]);
-
-//     return {
-//       ...loan,
-//       book: {
-//         id: bookResponse.data.id,
-//         title: bookResponse.data.title,
-//         author: bookResponse.data.author,
-//       },
-//       user: {
-//         id: userResponse.data.id,
-//         name: userResponse.data.name,
-//         email: userResponse.data.email,
-//       },
-//     };
-//   } catch (error) {
-//     throw error;
-//   }
-// }
